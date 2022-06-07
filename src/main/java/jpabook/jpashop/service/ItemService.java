d8c30baeaeced45300820b9de28824aabd6c3969
ones1kk <ones1k95@gmail.com>
2022-06-08 00:09:23 +0900

    ⚡️ Add : UpdateItem(dirty checking)

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 import java.util.List;
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,21 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    /**
+     * 변경감지(dirty checking)
+     * @param itemId
+     * @param name
+     * @param price
+     * @param stockQuantity
+     */
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity ) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public Item findItem(Long id) {
