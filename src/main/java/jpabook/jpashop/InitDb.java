f20ad5c8708fbbffe64773d2dc78786f56ca59da
ones1kk <ones1k95@gmail.com>
2022-06-09 00:35:28 +0900

    ✨ Add : InitData

--- src/main/java/jpabook/jpashop/InitDb.java
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -21,6 +21,7 @@ public class InitDb {
     @PostConstruct
     public void init() {
         service.dbInit1();
+        service.dbInit2();
     }
 
     @Component
@@ -34,29 +35,55 @@ public class InitDb {
             Member memberA = new Member();
             memberA.setName("userA");
             memberA.setAddress(new Address("aa", "aa", "aa"));
+            em.persist(memberA);
 
             Book bookA = new Book();
             bookA.setName("A");
             bookA.setPrice(10000);
             bookA.setStockQuantity(100);
-
             em.persist(bookA);
 
             Book bookB = new Book();
-            bookB.setName("A");
+            bookB.setName("B");
             bookB.setPrice(20000);
-            bookB.setStockQuantity(100);
-
+            bookB.setStockQuantity(200);
             em.persist(bookB);
-
-            em.persist(memberA);
 
             OrderItem orderItem1 = OrderItem.createOrderItem(bookA, 10000, 1);
             OrderItem orderItem2 = OrderItem.createOrderItem(bookB, 40000, 2);
 
             Delivery delivery = new Delivery();
             delivery.setAddress(memberA.getAddress());
-            Order.createOrder(memberA, delivery, orderItem1, orderItem2);
+            Order order = Order.createOrder(memberA, delivery, orderItem1, orderItem2);
+            em.persist(order);
         }
+
+        public void dbInit2() {
+            Member memberB= new Member();
+            memberB.setName("userB");
+            memberB.setAddress(new Address("bb", "bb", "bb"));
+            em.persist(memberB);
+
+            Book bookC = new Book();
+            bookC.setName("C");
+            bookC.setPrice(10000);
+            bookC.setStockQuantity(300);
+            em.persist(bookC);
+
+            Book bookD = new Book();
+            bookD.setName("D");
+            bookD.setPrice(20000);
+            bookD.setStockQuantity(400);
+            em.persist(bookD);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(bookC, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(bookD, 40000, 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(memberB.getAddress());
+            Order order = Order.createOrder(memberB, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
     }
 }
