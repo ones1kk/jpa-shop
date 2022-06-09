3dbaf802c18d2e1dfc590809ddb32ad59692068f
ones1kk <ones1k95@gmail.com>
2022-06-10 00:04:15 +0900

    ✨ Add : OrderApiV3

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -33,6 +33,15 @@ public class OrderApiController {
             .collect(Collectors.toList());
     }
 
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+
+        return orders.stream()
+            .map(SimpleOrderDto::new)
+            .collect(Collectors.toList());
+    }
+
     @Data
     static class SimpleOrderDto {
 
--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -18,6 +18,7 @@ public class Delivery {
 
     @Id
     @GeneratedValue
+
     @Column(name = "delivery_id")
     private Long id;
 
