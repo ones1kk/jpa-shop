a8548097c7cad33b51bc35afd921ecfe06f2de6b
ones1kk <ones1k95@gmail.com>
2022-06-15 00:10:47 +0900

    ✨ Add : OrderApiV1,2,3

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
@@ -14,51 +15,77 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/**
- * xToOne (ManyToOne, OneToOne) Order -> Member Order -> Delivery
- */
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
 
-    @GetMapping("/api/v1/simple-orders")
+    @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
-        return orderRepository.findAll(new OrderSearch());
+        List<Order> all = orderRepository.findAll(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.forEach(o -> o.getItem().getName());
+        }
+
+        return all;
     }
 
-    @GetMapping("/api/v2/simple-orders")
-    public List<SimpleOrderDto> ordersV2() {
-        return orderRepository.findAll(new OrderSearch()).stream().map(SimpleOrderDto::new)
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+
+        return orders.stream().map(OrderDto::new)
                 .collect(Collectors.toList());
     }
 
-    @GetMapping("/api/v3/simple-orders")
-    public List<SimpleOrderDto> ordersV3() {
-        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+    // OneToMany -> Paging 절대 불가능
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> orderV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
 
-        return orders.stream()
-                .map(SimpleOrderDto::new)
+        return orders.stream().map(OrderDto::new)
                 .collect(Collectors.toList());
     }
 
     @Data
-    static class SimpleOrderDto {
+    static class OrderDto {
 
         private Long orderId;
         private String name;
-        private LocalDateTime orderDate;
+        private LocalDateTime orderDatetime;
         private OrderStatus orderStatus;
         private Address address;
+        private List<OrderItemDto> orderItems;
 
-        public SimpleOrderDto(Order order) {
+        public OrderDto(Order order) {
             this.orderId = order.getId();
             this.name = order.getMember().getName();
-            this.orderDate = order.getOrderDateTime();
+            this.orderDatetime = order.getOrderDateTime();
             this.orderStatus = order.getStatus();
             this.address = order.getDelivery().getAddress();
+            this.orderItems = order.getOrderItems().stream().map(OrderItemDto::new).collect(Collectors.toList());
         }
+
+    }
+
+    @Data
+    static class OrderItemDto {
+
+        private String itemName; // 상품명
+        private int orderPrice; // 주문 가격
+        private int count; // 주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            this.itemName = orderItem.getItem().getName();
+            this.orderPrice = orderItem.getOrderPrice();
+            this.count = orderItem.getCount();
+        }
+
     }
 
 }
